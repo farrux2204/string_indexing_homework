36e805299127cc30b9952a79d1829d83d5dbb416
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-19 08:58:26 +0500

    08

--- Index08.py
+++ Index08.py
@@ -6,5 +6,22 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    a=s[0]
+    b=s[1]
+    c=s[2]
+    d=s[3]
+    f=s[4]
+    if a=="*":
+        return 0
+    elif b=="*":
+        return 1
+    elif c=="*":
+        return 2
+    elif d=="*":
+        return 3
+    elif f=="*":
+        return 4
+    else:
+        return False
+print(main("salom"))
         
