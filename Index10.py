0d1a24ba390e949b559db89152faf45e05447bcf
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-19 09:24:59 +0500

    0110

--- Index10.py
+++ Index10.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    a=int(s[0])
+    b=int(s[1])
+    c=int(s[2])
+    d=int(s[3])
+    e=int(s[4])
+    return a+b+c+d+e
+print(main('10005'))
